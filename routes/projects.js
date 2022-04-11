d39bda6b58ab956a519bb2392d0ce4f4c77e0200
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-04-11 14:10:12 +0200

    Forks gerandomized

--- routes/projects.js
+++ routes/projects.js
@@ -4,6 +4,24 @@ const graphqlAuth = graphql.defaults({
   headers: { authorization: 'token ' + process.env.GITHUB_PERSONAL_ACCESS_TOKEN },
 })
 
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+  }
+
+  return array;
+}
+
 module.exports = express
   .Router()
 
@@ -30,6 +48,10 @@ module.exports = express
         }
       }
     }`).then((data) => {
+      data.search.nodes.forEach((_element, index) => {
+        shuffle(data.search.nodes[index].forks.nodes)
+      })
+
       res.render('projects', {
         projects: data.search.nodes,
       })
