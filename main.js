2bfc072a61931630eb8ae7d1daceda2da595c60f
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-04-12 10:49:11 +0200

    Bestanden netter gemaakt

--- main.js
+++ main.js
@@ -1,37 +1,35 @@
 // Import Dotenv.
-require('dotenv').config()
+require("dotenv").config()
 // Import Express.
-const express = require('express')
+const express = require("express")
 // Import Handlebars.
-const hbs  = require('express-handlebars')
-const handlebars = hbs.engine
+const handlebars  = require("express-handlebars")
 // Import fs (file system).
 const fs = require("fs")
 // Import GraphQL.
-const { graphql } = require('@octokit/graphql')
+const { graphql } = require("@octokit/graphql")
 
 // Initialise Express.
 const app = express()
 
 // Configure GraphQL authorization.
 const graphqlAuth = graphql.defaults({
-  headers: { authorization: 'token ' + process.env.GITHUB_PERSONAL_ACCESS_TOKEN },
+  headers: { authorization: "token " + process.env.GITHUB_PERSONAL_ACCESS_TOKEN },
 })
 
 // Render static files.
 app.use(express.static("static"))
 
 // Set the view engine to Handlebars and change the filename extension.
-app.engine('hbs', handlebars({ extname: '.hbs' }))
-app.set('view engine', 'hbs')
-app.set('views', './views')
+app.engine("hbs", handlebars.engine({ extname: ".hbs" }))
+app.set("view engine", "hbs")
 
 // Set and log the port for Express.
 const port = process.env.PORT || 3000
 app.listen(port, () => { console.log(`Express running at http://localhost:${port}.`) })
 
 // Listen to all GET requests on /.
-app.get('/', (_req, res) => {
+app.get("/", (_req, res) => {
   // Get all cmda-minor-web repositories with 2122 in the name.
   graphqlAuth(`{
     search(query: "2122 org:cmda-minor-web", type: REPOSITORY, first: 20) {
@@ -56,7 +54,7 @@ app.get('/', (_req, res) => {
     }
   }`).then((data) => {
     // Load the index page with the subjects.
-    res.render('index', {
+    res.render("index", {
       subjects: data.search.nodes
     })
   })
@@ -80,7 +78,7 @@ function shuffle(array) {
   return array
 }
 
-app.get('/:subject', (req, res) => {
+app.get("/:subject", (req, res) => {
   // Get the cmda-minor-web repository that matches the subject name.
   graphqlAuth(`{
     search(query: "${req.params.subject} org:cmda-minor-web", type: REPOSITORY, first: 20) {
@@ -111,7 +109,7 @@ app.get('/:subject', (req, res) => {
     }
 
     // Render the subject page with the forks.
-    res.render('subject', {
+    res.render("subject", {
       forks: JSON.parse(fs.readFileSync(`static/json/${data.search.nodes[0].name}.json`, "utf8"))
     })
   })
